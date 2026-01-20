40f17111534cf7ef05cd89498390d7656703f6bd
Daniel Pérez <daniel.mailprof@gmail.com>
2026-01-20 11:32:08 +0100

    update

--- config.js
+++ config.js
@@ -3,8 +3,9 @@ console.log("config");
 console.log("cuadrado rojo");
 
 
-let multiplicar  = (a, b) => {
-  return a * b
-}
+let multiplicar  = (a, b) => a * b
 
+let myArr = ["Dickson", "Mayass", "Rosa", "Melano"]
+
+console.log(myArr[2] + "," + myArr[3]);
 console.log(multiplicar(30*40));
