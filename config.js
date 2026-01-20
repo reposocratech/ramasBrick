bd6559941dd26fefe483981314c9848b7cf56503
Javier Sanz <javiersanzvilloldo@gmail.com>
2026-01-20 10:36:28 +0100

    prueba

--- config.js
+++ config.js
@@ -1,3 +1,12 @@
 console.log("config");
 
 console.log("cuadrado rojo");
+
+let restar = (a, b) => a - b;
+let dividir = (a, b) => a / b;
+let multiplicar = (a, b) => a * b;
+
+console.log(restar(4, 2));
+console.log(dividir(4, 2));
+console.log(multiplicar(4, 2));
+
