bac7878ee1ca05e4fc214603e5cc76c561e8b402
Elena <elenaptlaboral@gmail.com>
2026-01-20 10:36:50 +0100

    cesta de la compra

--- pepito.js
+++ pepito.js
@@ -1,1 +1,6 @@
 console.log("pepito");
+
+
+let cestaDeLaCompra = ["manzanas", "peras", "pescado", "leche", "huevos"]
+console.log("Esto es una prueba")
+console.log("Y esta es mi cesta de la compra: " + cestaDeLaCompra)
