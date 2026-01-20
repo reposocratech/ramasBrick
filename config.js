feff3e2d9a6f96d045d0a3c2f6e310668798547b
Vicent Torres <vicentorres22@gmail.com>
2026-01-20 10:39:38 +0100

    probando git

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 console.log("config");
 
 console.log("cuadrado rojo");
+
+console.log("Probando git desde la rama Vicent")
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 console.log("Hola caracola!!!");
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
