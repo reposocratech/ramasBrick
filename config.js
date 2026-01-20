b11f1af2bac702827dcba5d6a83c8083f25a8092
JuancokTrj <juancarlostorreswork@gmail.com>
2026-01-20 10:36:30 +0100

    a Santi el mejor

--- config.js
+++ config.js
@@ -1,3 +1,12 @@
 console.log("config");
 
 console.log("cuadrado rojo");
+
+
+function restar(a, b) {
+  return a - b;
+}
+const resultado = restar(5, 3);
+
+console.log("El resultado de la resta es:" + resultado);
+const restar = (a, b) => a - b;
